75c5eba6a79c5860ca6be6f2cae2b0948079766b
tony lopez <lopez911@anthonys-mbp.lan>
2023-01-12 02:34:47 -0500

    init + added a dummy s3 resource

--- lib/lni-serverless-backend-cdk-stack.ts
+++ lib/lni-serverless-backend-cdk-stack.ts
@@ -1,10 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';''
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LniServerlessBackendCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const lniBucket: Bucket = new Bucket(this, 'lni-bucket', {
+      bucketName: "lni-bucket"
+    });
 
     // The code that defines your stack goes here
 
