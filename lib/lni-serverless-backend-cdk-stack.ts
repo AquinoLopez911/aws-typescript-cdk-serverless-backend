01990efb6cf0f74d8df1dc4063f5e58830ba6d82
tony lopez <lopez911@anthonys-mbp.lan>
2023-01-12 04:03:13 -0500

    added a lambda function and a get method integration with the lniApi(restApi)

--- lib/lni-serverless-backend-cdk-stack.ts
+++ lib/lni-serverless-backend-cdk-stack.ts
@@ -1,21 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Bucket } from 'aws-cdk-lib/aws-s3';''
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Code, Function as LamFunc, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
 
 export class LniServerlessBackendCdkStack extends cdk.Stack {
+
+  private api: RestApi = new RestApi(this, "lniApi")
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const lniBucket: Bucket = new Bucket(this, 'lni-bucket', {
-      bucketName: "lni-bucket"
-    });
+    const helloLambda: LamFunc = new LamFunc(this, "helloLambda", {
+      functionName: "helloLambda",
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
+      handler: "hello.handler"
+    })
 
-    // The code that defines your stack goes here
+    // lniApi lambda initegratioin
+    // allows a client to call the helloLambda function 
+    const helloLambdaResource = this.api.root.addResource('hello');
+    const helloLambdaIntegration:LambdaIntegration = new LambdaIntegration(helloLambda);
+    helloLambdaResource.addMethod('GET', helloLambdaIntegration)
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LniServerlessBackendCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
