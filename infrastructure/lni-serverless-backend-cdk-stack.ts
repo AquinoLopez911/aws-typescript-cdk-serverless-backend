59beff0504b2adb6301ad59063abfffe41984ec9
aquinolopez911 <aquinolopeza911@gmail.com>
2023-01-13 01:55:46 -0500

    ^

--- infrastructure/lni-serverless-backend-cdk-stack.ts
+++ infrastructure/lni-serverless-backend-cdk-stack.ts
@@ -1,24 +1,37 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
-import { Code, Function as LamFunc, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
 import { GenericTable } from './GenericTable';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class LniServerlessBackendCdkStack extends cdk.Stack {
 
   private api: RestApi = new RestApi(this, "lniApi")
-  private dynamoTables = new GenericTable('lniTable', 'lniId', this)
+  // private dynamoTables = new GenericTable('lniTable', 'lniId', this)
+  private spacesTable = new GenericTable(this, {
+    tableName: 'SpacesTable',
+    primaryKey: 'spaceId',
+    createLambdaPath: 'Create'
+  }) 
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // creates allow policy statement
+    const s3ListPolicy: PolicyStatement = new PolicyStatement();
+    s3ListPolicy.addActions('s3:listAllMyBuckets');
+    s3ListPolicy.addResources('*');
+
+    // creates a lambda function with functionallity to write code in .ts, does the compliling behind the scenes to a .js file
     const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
       functionName: "holaLambda",
       entry: join(__dirname, "..", "services", "node-lambda", 'hello.ts'),
       handler: "handler"
     })
+    // add allow s3 list action policy to the helloLambdaNodeJs function
+    helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
 
     // lniApi lambda initegratioin
     // allows a client to call the helloLambdaNodeJs function 
@@ -26,5 +39,8 @@ export class LniServerlessBackendCdkStack extends cdk.Stack {
     const helloLambdaNodeJsIntegration: LambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
     helloLambdaNodeJsResource.addMethod('GET', helloLambdaNodeJsIntegration)
 
+    //spaces api integrations:
+    const spaceResouce = this.api.root.addResource('spaces');
+    spaceResouce.addMethod('POST', this.spacesTable.createLambdaIntegration);
   }
 }
--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,13 +1,21 @@
 import {v4} from 'uuid'
+import { S3 } from 'aws-sdk'
+import { Handler } from 'aws-cdk-lib/aws-lambda';
 
-exports.handler = async (event: any, context: any, callback: any) => {
+const s3Client: S3 = new S3();
+
+const handler = async (event: any, context: any, callback: any) => {
     console.log(event);
+
+    const buckets = await s3Client.listBuckets().promise();
 
     return {
         headers: {
             'Content-Type': 'application/json'
         },
         statusCode: 200,
-        body: "hello from serverless " + v4()
+        body: 'here are your buckets ' + JSON.stringify(buckets.Buckets, null, 2)
     }
 }
+
+export { handler }
