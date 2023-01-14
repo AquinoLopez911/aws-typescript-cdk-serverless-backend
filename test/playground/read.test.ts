1ef94b6386c829a381325cdb266a362768ddaac7
aquinolopez911 <aquinolopeza911@gmail.com>
2023-01-14 17:26:09 -0500

    *added a get one endpoint in the http file. * added the PRRIMARY_KEY to the launch.json env variables * added a check in the read lambda to return a single entry if there is a primary key in the venet parameters. *added a test for get one in the read lambda test file

--- test/playground/read.test.ts
+++ test/playground/read.test.ts
@@ -1,12 +1,20 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
 import { handler } from '../../services/SpacesTableLambdas/Read'
 
-const event = {
-    body: {
-        
+const event: APIGatewayProxyEvent = {
+    queryStringParameters: {
+        spaceId: "c0ae2d09-0e4d-40a0-bcee-dc8fd289a550"
     }
-}
+} as any
 
-const res = handler({} as any, {} as any).then(apiRes => {
+// get one 
+const res = handler(event, {} as any).then(apiRes => {
+    const items = JSON.parse(apiRes.body);
+    console.log(items)
+} );
+
+// scan 
+const res2 = handler({} as any, {} as any).then(apiRes => {
     const items = JSON.parse(apiRes.body);
     console.log(items)
 } );
