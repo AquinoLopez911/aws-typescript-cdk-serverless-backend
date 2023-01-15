b383c0240f04618959d55144c556bb0d19ca5e51
aquinolopez911 <aquinolopeza911@gmail.com>
2023-01-15 15:38:41 -0500

    *added a test for getting all items with specified secondary index value *added a http request to test for get all by secondary index

--- test/playground/read.test.ts
+++ test/playground/read.test.ts
@@ -1,20 +1,35 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { handler } from '../../services/SpacesTableLambdas/Read'
 
+// // getOneWithPrimaryKey
 const event: APIGatewayProxyEvent = {
     queryStringParameters: {
         spaceId: "c0ae2d09-0e4d-40a0-bcee-dc8fd289a550"
     }
 } as any
 
-// get one 
 const res = handler(event, {} as any).then(apiRes => {
     const items = JSON.parse(apiRes.body);
     console.log(items)
 } );
 
-// scan 
-const res2 = handler({} as any, {} as any).then(apiRes => {
+
+//getAllWithSecondaryKey
+const event2: APIGatewayProxyEvent = {
+    queryStringParameters: {
+        location: "gateway"
+    }
+} as any
+
+const res2 = handler(event2, {} as any).then(apiRes => {
+    console.log(apiRes)
+    const items = JSON.parse(apiRes.body);
+    console.log(items)
+} );
+
+
+// // scan 
+const res3 = handler({} as any, {} as any).then(apiRes => {
     const items = JSON.parse(apiRes.body);
     console.log(items)
 } );
